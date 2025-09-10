1b1180dc1948aaa7a9f7dc01b159d3d33d950dbe
Pablo Enoc <pablo@enocc.com>
2025-09-10 16:33:19 -0700

    timer function

--- timer.js
+++ timer.js
@@ -1,1 +1,46 @@
 console.log("Timer script ready to start...");
+
+function timer() {
+    return {
+        minutes: 0,
+        seconds: 0,
+        intervalId: null,
+
+        start() {
+            this.minutes = 0;
+            this.seconds = 0;
+
+            if (this.intervalId !== null) {
+                clearInterval(this.intervalId);
+                this.intervalId = null;
+            }
+
+            this.intervalId = setInterval(() => {
+                if (this.seconds === 0) {
+                    if (this.minutes === 0) {
+                        clearInterval(this.intervalId);
+                        this.intervalId = null;
+                        this.onFinish(); // TODO
+                        return;
+                    }
+                    this.minutes--;
+                    this.seconds = 59;
+                } else {
+                    this.seconds--;
+                }
+            }, 1000);
+        },
+
+        stop() {
+            if (this.intervalId !== null) {
+                clearInterval(this.intervalId);
+                this.intervalId = null;
+            }
+        },
+
+        onFinish() {
+            console.log("Timer finished");
+            // TODO: Notifications
+        }
+    }
+}
