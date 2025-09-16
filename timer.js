605b2d2a2b3d5124f24d4b7257310b8bf5d901cf
Pablo Enoc <pablo@enocc.com>
2025-09-16 16:49:21 -0700

    relative url

--- timer.js
+++ timer.js
@@ -47,7 +47,7 @@ function timer() {
 }
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/sw.js")
+    navigator.serviceWorker.register("sw.js")
         .then(reg => {
             console.log("Service worker registered.");
         })
