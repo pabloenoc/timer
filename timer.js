6df970a7a06122328f1f4c3a408539bb18822b02
Pablo Enoc <pablo@enocc.com>
2025-09-16 16:40:38 -0700

    send notification when timer done

--- timer.js
+++ timer.js
@@ -41,19 +41,45 @@ function timer() {
 
         onFinish() {
             console.log("Timer finished");
-            // TODO: Notifications
+            sendNotification();
         }
     }
 }
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register("/sw.js")
-    .then(reg => {
-        console.log("Service worker registered.");
-    })
-    .catch(err => {
-        console.error("ServiceWorker registration failed.");
-    })
+        .then(reg => {
+            console.log("Service worker registered.");
+        })
+        .catch(err => {
+            console.error("ServiceWorker registration failed.");
+        })
 } else {
     console.log('There is no service worker support in this browser.');
+}
+
+async function sendNotification() {
+    if (!('Notification' in window)) return console.log('Notifications not supported');
+    if (Notification.permission !== 'granted') return console.log('Permission for notifactions not granted yet');
+
+    try {
+
+        options = {
+            body: 'Timer is up!',
+            icon: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(
+                `<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128">
+     <rect width="100%" height="100%" fill="#ffffff"/>
+     <text x="50%" y="54%" dominant-baseline="middle" text-anchor="middle" font-size="64">🚧</text>
+   </svg>`
+            ),
+            renotify: false
+
+        }
+
+        const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+        serviceWorkerRegistration.showNotification('(Elias Timer) Task Completed', options);
+        console.log('Notification requested');
+    } catch (err) {
+        console.log('Failed to show notification:', err);
+    }
 }
